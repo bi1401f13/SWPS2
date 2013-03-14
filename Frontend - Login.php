67cc958b6af94581f738c6f3b992dd2dab28ed9f
Andreas Lundgaard <andreaslund91@gmail.com>
2013-03-14 13:35:24 +0100

    Revert "Revert "Ændret navne""
    This reverts commit 059ebbc8329ae4d61533e96cbe815b7df1766074.

---
+++ Frontend - Login.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>CTRL-ALL-SHIFTS</title>
+</head>
+
+<body>
+	<form action="test_login.php" method="post" enctype="multipart/form-data">
+		<label>Username:</label> 
+		<input type="text" value="" />
+
+		<label>Password:</label>	
+		<input type="password" value="" />
+
+		<input type="submit" value="Login" />
+	</form>
+</body>
+</html>
