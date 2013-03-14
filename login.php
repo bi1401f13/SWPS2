ccdcfd0444a00dbb35808c8a2870e91529d9d2d4
rasmus_m_jakobsen@hotmail.com <rasmus_m_jakobsen@hotmail.comcd c:ddgit config --global user.email rasmus_m_jakobsen@hotmail.com>
2013-03-14 15:54:17 +0100

    fixes med login

--- login.php
+++ login.php
@@ -1,16 +1,32 @@
 <?php 
+
+session_start();
 // load database login data
 require_once 'dbconnect.php';
 
+//Test basic sql connection
+
+$db_server = mysql_connect($db_host, $db_username, $db_password);
+if (!$db_server) {
+die("Kan ikke forbinde til MySQL: " . mysql_error());
+} 
+
+
 // connect to mysql database
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
+mysql_connect("$db_host", "$db_username", "$db_password")or die("cannot connect");
 mysql_select_db("$db_name")or die("cannot select DB");
 
+
+
+
 // define username and password as variables
-$username=$_POST['emp_username'];
-$password=$_POST['emp_password']; 
+$username=$_POST['empUsername'];
+$password=$_POST['empPassword']; 
+
+
 
 // mysql security protection (injection cleaner)
+$username = stripslashes($username);
 $username = stripslashes($username);
 $password = stripslashes($password);
 $username = mysql_real_escape_string($username);
@@ -18,16 +34,16 @@ $password = mysql_real_escape_string($password);
 
 // check login information from login window to database for verification
 $sql="SELECT * FROM $tbl_name WHERE username='$username' and password='$password'";
-$result=mysqli_query($sql);
+$result=mysql_query($sql);
 
 // counts number of rows in the result to ensure that the inputdata is only one row
-$count=mysqli_stmt_num_rows($result);
+$count=mysql_num_rows($result);
 if($count==1){
 
 // redirect if login is successfull
+$_SESSION['username'];
+$_SESSION['password'];
 
-session_register("username");
-session_register("password");
 header("location:login_success.php");
 }
 
@@ -35,4 +51,14 @@ header("location:login_success.php");
 else {
 echo "Wrong Username or Password";
 }
+
+
+
 ?>
+
+
+
+
+
+
+
