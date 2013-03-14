45bc28d115596c76482fcad47de227d10ab2435a
Andreas Lundgaard <andreaslund91@gmail.com>
2013-03-14 13:02:33 +0100

    Frontend til login side

---
+++ Frontend - Login.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>CTRL-ALL-SHIFTS</title>
+</head>
+
+<body>
+	<form>
+		<label>Username:</label> 
+		<input type="text" value="" />
+
+		<label>Password:</label>	
+		<input type="password" value="" />
+
+		<input type="submit" value="Login" />
+	</form>
+</body>
+</html>
